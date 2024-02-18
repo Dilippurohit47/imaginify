2a586959d152b56263cc7def58bc94e418f975de
Dilippurohit47 <dilippurohit204@gmail.com>
2024-02-18 11:32:17 +0530

    push

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
 import Mobilenav from "@/components/shared/mobilenav";
 import Sidebar from "@/components/shared/sidebar";
+import { connectToDatabase } from "@/lib/database/mongoose";
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+
+  await connectToDatabase();
+
   return (
     <main className="root">
       <Sidebar/>
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,34 +1,35 @@
-import mongoose ,{Mongoose} from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
-const URL = process.env.MONGO_URL || " mongodb+srv://dilippurohit204:123456@dilip@cluster0.qjibzwv.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_URL = "mongodb+srv://dilippurohit204:123456dilip@cluster0.qjibzwv.mongodb.net/?retryWrites=true&w=majority";
 
-interface mongooseConnection {
-    conn : Mongoose | null;
-promise : Promise<Mongoose> | null;
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached: mongooseConnection=(global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose
 
 if(!cached) {
-    cached = (global as any).mongoose ={conn : null,  promise: null}
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-export const connectToDatabase = async()=>{
-    try {
-        if(cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
 
-        if(URL) throw new Error("MOngo db url is not defined ")
-    
-    
-        cached.promise = cached.promise || mongoose.connect(URL , {dbName:"Imaginify",bufferCommands:false})
-    
-        cached.conn = await cached.promise;
-    console.log("connected to mongo db succesfully")
-        return cached.conn
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-        throw error; // Re-throw the error to let the caller handle it
-    }
 
+  if(cached.conn) return cached.conn;
+
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'Imaginify', bufferCommands: false 
+    })
+console.log("mongo coonected ")
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 
 }
